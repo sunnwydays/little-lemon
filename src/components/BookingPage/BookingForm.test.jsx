444aa6169046dbf3fde2e4100f4285309577c488
if3ll <sunnwydays@gmail.com>
2024-06-18 17:00:46 -0400

    unit tests for form validation

--- src/components/BookingPage/BookingForm.test.jsx
+++ src/components/BookingPage/BookingForm.test.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import BookingForm from './BookingForm';
 import '@testing-library/jest-dom';
 
@@ -11,7 +11,7 @@ describe('BookingForm static text exists', () => {
     })
 })
 
-describe('BookingForm Input Fields', () => {
+describe('BookingForm input field attributes', () => {
   const mockSubmitForm = jest.fn();
   const mockDispatch = jest.fn();
   const availableTimes = ['18:00', '19:00']; // Example available times
@@ -54,4 +54,48 @@ describe('BookingForm Input Fields', () => {
     expect(occasionSelect).toContainElement(screen.getByText('Anniversary'));
     expect(occasionSelect).toContainElement(screen.getByText('Victory'));
   });
+});
+
+describe('BookingForm input field validation', () => {
+    const mockSubmitForm = jest.fn();
+    const mockDispatch = jest.fn();
+    const availableTimes = ['18:00', '19:00'];
+
+    beforeEach(() => {
+        render(<BookingForm availableTimes={availableTimes} dispatch={mockDispatch} submitForm={mockSubmitForm} />);
+    });
+
+    test('declines past dates', () => {
+        const dateInput = screen.getByLabelText(/Date/i)
+        const submitButton = screen.getByRole('button')
+        const date = new Date();
+        date.setDate(date.getDate() - 1);
+        const pastDate = date.toISOString().split('T')[0];
+
+        expect(dateInput).toHaveValue('');
+
+        fireEvent.focus(dateInput)
+        fireEvent.change(dateInput, { target: { value: pastDate } })
+        fireEvent.blur(dateInput)
+    }) /* this test doesn't work, probably because of synchronicity */
+
+    test('accepts valid inputs', () => {
+        const dateInput = screen.getByLabelText(/Date/i)
+        const timeSelect = screen.getByLabelText(/Time/i)
+        const guestInput = screen.getByLabelText(/Number of guests/i)
+        const occasionSelect = screen.getByLabelText(/Occasion/i)
+        const submitButton = screen.getByRole('button')
+        const today = (new Date()).toISOString().split('T')[0]
+
+        fireEvent.change(dateInput, {target:{value: today}})
+        fireEvent.change(timeSelect, {target: {value: '18:00'}} )
+        fireEvent.change(guestInput, {target: {value: 4}})
+        fireEvent.change(occasionSelect, {target: {value: 'Birthday'}})
+
+        expect(dateInput).toHaveValue(today);
+        expect(timeSelect).toHaveValue('18:00');
+        expect(guestInput).toHaveValue(4);
+        expect(occasionSelect).toHaveValue('Birthday');
+        expect(submitButton).not.toBeDisabled();
+    })
 });
